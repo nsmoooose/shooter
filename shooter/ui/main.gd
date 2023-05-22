97b400b70271e0adcb80cca697c9d4f4a6cfafa5
Henrik Nilsson <nsmoooose@gmail.com>
2023-05-22 17:16:44 +0200

    Start now loads the first level

--- shooter/ui/main.gd
+++ shooter/ui/main.gd
@@ -1,7 +1,21 @@
 extends Node3D
 
+func load_level(level_name: String):
+	for x in $Level.get_children() + $HUD.get_children():
+		x.queue_free()
+	
+	var scene = load(level_name)
+	var instance = scene.instantiate()
+	$Level.add_child(instance)
+	
+	var player = load("res://player.tscn")
+	instance = player.instantiate()
+	$Level.add_child(instance)
+	
+	$Camera3D.queue_free()
+
 func _on_hud_game_quit():
 	get_tree().quit()
 
 func _on_hud_game_start():
-	get_tree().change_scene_to_file("res://game.tscn")
+	load_level("res://levels/level_01.tscn")
