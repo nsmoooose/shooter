4dccb0a62843d9729c160e7c2ef044a19214d581
Henrik Nilsson <nsmoooose@gmail.com>
2023-06-16 21:43:56 +0200

    Basic networking support

--- shooter/ui/main.gd
+++ shooter/ui/main.gd
@@ -1,20 +1,20 @@
 extends Node3D
 
+const PORT:int = 4433
+
 func load_level(level_name: String):
-	for x in $Level.get_children() + $HUD.get_children():
+	for x in $Network.get_children():
 		x.queue_free()
 	
 	var scene:Resource = load(level_name)
 	var instance:Node3D = scene.instantiate()
-	$Level.add_child(instance)
+	$Network.add_child(instance)
 	
 	var player:Resource = load("res://player.tscn")
 	var player_instance = player.instantiate()
 	player_instance.pause.connect(_on_pause)
 	player_instance.unpause.connect(_on_unpause)
-	$Players.add_child(player_instance)
-	
-	$Camera3D.queue_free()
+	$Network.add_child(player_instance)
 	
 func _on_pause():
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
@@ -34,6 +34,31 @@ func _on_hud_game_quit():
 	get_tree().quit()
 
 func _on_hud_game_start():
+	for x in $Network.get_children() + $HUD.get_children():
+		x.queue_free()
+	$Camera3D.queue_free()
+
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_server(PORT)
+	if peer.get_connection_status() == MultiplayerPeer.CONNECTION_DISCONNECTED:
+		OS.alert("Failed to start multiplayer server.")
+		return
+	multiplayer.multiplayer_peer = peer
+
 	load_level("res://levels/level_01.tscn")
-	
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+func _on_hud_game_join():
+	for x in $Network.get_children() + $HUD.get_children():
+		x.queue_free()
+	$Camera3D.queue_free()
+
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_client("127.0.0.1", PORT)
+	if peer.get_connection_status() == MultiplayerPeer.CONNECTION_DISCONNECTED:
+		OS.alert("Failed to start multiplayer client.")
+		return
+
+	multiplayer.multiplayer_peer = peer
--- shooter/ui/main_menu_hud.gd
+++ shooter/ui/main_menu_hud.gd
@@ -1,14 +1,22 @@
 extends CanvasLayer
 
 signal game_start
+signal game_join
 signal game_options
 signal game_quit
+
 
 func _on_start_button_button_down():
 	game_start.emit()
 
+
 func _on_settings_button_button_down():
 	game_options.emit()
 
+
 func _on_quit_button_button_down():
 	game_quit.emit()
+
+
+func _on_join_server_button_button_down():
+	game_join.emit()
