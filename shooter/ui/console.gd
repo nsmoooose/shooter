a4d67ccde0b1f8bf07f13473dc3dde714d7b547c
Henrik Nilsson <nsmoooose@gmail.com>
2023-07-02 14:23:01 +0200

    Implement some simple console commands

--- shooter/ui/console.gd
+++ shooter/ui/console.gd
@@ -1,18 +1,35 @@
 extends CanvasLayer
 
 @onready var command = $VSplitContainer/Panel/VBoxContainer/HBoxContainer/CommandTextEdit
+@onready var history = $VSplitContainer/Panel/VBoxContainer/History
 
 signal console_close
 
+var commands = {}
 
 func console_activate():
 	visible = true
 	command.grab_focus()
 
 
-# Called when the node enters the scene tree for the first time.
+func cmd_help():
+	for name in commands:
+		history.add_text(name + ":  " + commands[name]["desc"] + "\n")
+
+
+func cmd_quit():
+	get_tree().quit()
+
+
+func cmd_exit():
+	console_close.emit()
+
+
 func _ready():
-	pass # Replace with function body.
+	commands["exit"] = {"call": cmd_exit, "desc": "Exits the console"}
+	commands["help"] = {"call": cmd_help, "desc": "Shows help about known commands"}
+	commands["quit"] = {"call": cmd_quit, "desc": "Quits this game"}
+
 
 func _input(event):
 	if command.has_focus() and event is InputEventKey and event.is_pressed():
@@ -23,4 +40,10 @@ func _input(event):
 		if event.keycode == KEY_ENTER:
 			get_viewport().set_input_as_handled()
 
-			# TODO Process the text entered
+			var cmd = command.text
+			command.text = ""
+
+			if cmd in commands:
+				commands[cmd]["call"].call()
+			else:
+				history.add_text("ERROR: '" + cmd + "' is not a known command.\n")
